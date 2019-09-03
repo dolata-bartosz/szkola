3c6767408645b133140f55c14df37880502c0f12
dolata-bartosz <bartekdolata123@wp.pl>
2019-09-03 15:43:54 +0200

    second file

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+      $imie='John';
+      $imie1='Kate';;
+     ?>
+  </body>
+</html>
