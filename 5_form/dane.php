6da4ea574104c7c025e1eb302f6ec06e6a4a83bb
dolata-bartosz <>
2019-10-01 15:43:04 +0200

    lekcja 4

---
+++ 5_form/dane.php
@@ -0,0 +1,7 @@
+<?php
+  if(!isset($_POST['button'])){
+    header('location: ../5_form.php');
+  }else {
+    echo "ok";
+  }
+ ?>
---
+++ form.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pl" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <form action="./5_form/dane.php" method="post">
+      <input type="text" name="imie" placeholder="podaj imie"><br><br>
+      <input type="submit" name="button" value="wyslij">
+    </form>
+  </body>
+</html>
