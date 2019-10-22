7c25c9ca678c271e48282f7f67fc151159042164
dolata-bartosz <>
2019-10-22 15:39:05 +0200

    lekcja

---
+++ 6_petle/petle2.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="pl" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <style media="screen">
+      table, td{
+        border: 1px solid black;
+        border-collapse: collapse;
+        padding:10px;
+        margin:10px;
+      }
+    </style>
+  </head>
+  <body>
+      <form method="post">
+        wiersze:<input type="number" name="number">
+        kolumny:<input type="number" name="number1">
+        <input type="submit" name="but" value="wyślij">
+      </form>
+      <table>
+      <?php
+        if(isset($_POST['but'])&&!empty($_POST['number'])&&!empty($_POST['number1'])){
+          $wiersze=$_POST['number'];
+          $kolumny=$_POST['number1'];
+          $i=1;
+          $j=1;
+          do{
+            echo '<tr>';
+            do{
+
+              if($i!=$j)$wynik=$i*$j;
+              else $wynik='*';
+              echo '<td>',$wynik,'</td>';
+              $j++;
+            }while($j<=$kolumny);
+            $j=1;
+            echo '</tr>';
+            $i++;
+          }while($i<=$wiersze);
+        }
+       ?>
+       </table>
+  </body>
+</html>
---
+++ 6_petle/petle3.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="pl" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <style media="screen">
+      table, td{
+        border: 1px solid black;
+        border-collapse: collapse;
+        padding:10px;
+        margin:10px;
+      }
+      .red{
+        background-color: red;
+      }
+    </style>
+  </head>
+  <body>
+      <form method="post">
+        wiersze:<input type="number" name="number">
+        kolumny:<input type="number" name="number1">
+        <input type="submit" name="but" value="wyślij">
+      </form>
+      <table>
+      <?php
+        if(isset($_POST['but'])&&!empty($_POST['number'])&&!empty($_POST['number1'])){
+          $wiersze=$_POST['number'];
+          $kolumny=$_POST['number1'];
+          $i=1;
+          $j=1;
+          $suma=0;
+          do{
+            echo '<tr>';
+            do{
+              $wynik=$i*$j;
+              if($i==$j){
+                echo '<td class="red">',$wynik,'</td>';
+                $suma+=$wynik;
+              }else echo '<td>',$wynik,'</td>';
+              $j++;
+            }while($j<=$kolumny);
+            $j=1;
+            echo '</tr>';
+            $i++;
+          }while($i<=$wiersze);
+
+       ?>
+       </table>
+       <?php
+        echo "suma danych przekatnej: ",$suma;}
+          ?>
+  </body>
+</html>
