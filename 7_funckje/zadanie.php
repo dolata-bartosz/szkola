65d0aa052a5f1691dcba54cd571763d0648f9743
Bartosz Dolata <54848431+dolata-bartosz@users.noreply.github.com>
2019-11-06 07:36:48 +0100

    send

--- 7_funckje/zadanie.php
+++ 7_funckje/zadanie.php
@@ -5,15 +5,6 @@
     <title></title>
   </head>
   <body>
-    <?php
-      function suma(){
-
-      }
-     ?>
-    <form method="post">
-      <input type="number" name="ilosc">
-      <input type="submit" name="bnt">
-      </form>
       <?php
         if(!empty($_POST['ilosc'])&&(isset($_POST['bnt'])||isset($_POST['bnt2']))){
           $ilosc = $_POST['ilosc'];
@@ -21,23 +12,31 @@
           if($ilosc>=1 && $ilosc <=70){
           ?>
             <form method="post">
+              <input style="display: none;" type="text" name="ilosc" value="<?php $ilosc ?>" >
               <?php
             for($i=1;$i<=$ilosc;$i++){
                 echo "<br>","<input type=\"number\" name=\"ilosc$i\">";
-            }
+                }
 
             ?>
             <input type="submit" name="bnt2">
           </form>
           <?php
-          for($i=1;$i<=$ilosc;$i++){
-            if(!empty($_POST["ilosc$i"])){
-              $t[$i] = $_POST["ilosc$i"];
+              for($i=1;$i<=$ilosc;$i++){
+                  if(!empty($_POST["ilosc$i"])){
+                      echo $_POST["ilosc$i"];
+                  }
                 }
-          }
+            }
+          } else {
+        ?>
+        <form method="post">
+          <input type="number" name="ilosc">
+          <input type="submit" name="bnt">
+          </form>
 
-        }
-        }
+        <?php
+          }
        ?>
 
   </body>
