6e1e4a0da98ca41cc93a871d93e6d587c4da2be0
dolata-bartosz <>
2019-12-03 15:36:48 +0100

    lekcja2

--- 11_baza/1_egzamin_E_14/index.php
+++ 11_baza/1_egzamin_E_14/index.php
@@ -17,5 +17,44 @@
     echo "</ul>";
     mysqli_close($connect);
      ?>
+     <form method="post">
+       <select name="sel">
+         <option value="c">Czekolada</option>
+         <option value="g">Grześki</option>
+         <option value="b">Baton</option>
+         <option value="w">Wafel</option>
+       </select>
+       <input type="submit" name="button" value="Wybierz">
+     </form>
+     <?php
+     if(isset($_POST["sel"])){
+       $towar = $_POST["sel"];
+       ############################################
+      switch ($towar) {
+        case 'c':
+          $towar = "Czekolada";
+          break;
+        case 'g':
+          $towar = "Grześki";
+          break;
+        case 'b':
+          $towar = "Baton";
+          break;
+        case 'w':
+          $towar = "Wafle";
+          break;
+      }
+      echo $towar;
+      $connect = mysqli_connect("localhost","root","","sklepik");
+      mysqli_set_charset($connect,"utf8");
+      $sql = "SELECT `cena` FROM `towary` WHERE `nazwa`='$towar'";
+      $result=mysqli_query($connect,$sql);
+      $row = mysqli_fetch_assoc($result);
+      $promocja = round($row["cena"] * 0.85,2);
+      echo " ".$promocja;
+      mysqli_close($connect);
+    }
+
+      ?>
   </body>
 </html>
