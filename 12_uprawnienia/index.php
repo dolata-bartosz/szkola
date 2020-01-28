f6d6dc822dbbabd12995d475a0e7ecae1c446263
Bartosz Dolata <54848431+dolata-bartosz@users.noreply.github.com>
2020-01-28 17:11:06 +0100

    lekcje
    cc

---
+++ 12_uprawnienia/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Baza - uprawnienai</title>
+    <link rel="stylesheet" href="./css/style.css">
+  </head>
+  <body>
+    <h3>Użytkownicy z tabeli user</h3>
+    <?php
+      require_once('./skrypty/connect.php');
+      require_once('./skrypty/function.php');
+      $sql =  "SELECT * FROM `user`";
+      $result = mysqli_query($conn,$sql);
+      echo<<<TABLE
+      <table>
+        <tr>
+          <th>Imie</th>
+          <th>Nazwisko</th>
+          <th>Data urodzenia</th>
+          <th>Rok</th>
+        </tr>
+TABLE;
+        while($row = mysqli_fetch_assoc($result)){
+          $year=year($row['bday']);
+          echo<<<ROW
+          <tr>
+            <td>$row[name]</td>
+            <td>$row[surname]</td>
+            <td>$row[bday]</td>
+            <td>$year</td>
+            <td><a href="./skrypty/del_user.php?id=$row[id]">Usuń</a></td>
+ROW;
+}
+    echo  "</table>";
+
+     ?>
+
+  </body>
+</html>
