cc6c9bcf967765fc4634210f8f86da923bd6c849
Bartosz Dolata <54848431+dolata-bartosz@users.noreply.github.com>
2020-02-24 21:48:42 +0100

    lesson 14

--- 14_update/connect.php
+++
@@ -1,5 +0,0 @@
-<?php
-  //$conn = mysqli_connect("localhost","root","","komis");
-  $conn = mysqli_connect("localhost","katarzyna","qweasd","komis");
-  mysqli_set_charset($conn,"utf8")
- ?>
--- 14_update/function.php
+++
@@ -1,5 +0,0 @@
-<?php
-function year($x){
-  return substr($x,0,4);
-}
- ?>
